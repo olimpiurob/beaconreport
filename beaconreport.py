612b8af9aa33f2a878a547faaab93e72afd641d3
Olimpiu Rob <olimpiu.rob@gmail.com>
2016-05-29 13:09:39 +0300

    Set beacon state (home/not_home) based on beacon activity

--- beaconreport.py
+++ beaconreport.py
@@ -30,22 +30,49 @@ class BeaconReport(BaseMQTTClient):
     """BeaconReport class"""
     def __init__(self, config_file='config'):
         super(BeaconReport, self).__init__(config_file=config_file)
+        self.beacons = {}
         self.scanner = Scanner()
 
-    def report(self, topic, data):
+    def report(self, beacon, data):
         if self.mqtt_client:
+            topic = self.topic_id + '%s' % beacon
             d = datetime.utcnow()
             timestamp = calendar.timegm(d.utctimetuple())
             data['tst'] = timestamp
+            self.beacons[beacon]['datetime'] = d
             self.mqtt_client.publish(topic, json.dumps(data))
 
     def do_scan(self):
         if self.scanner:
             for beacon, data in self.scanner.scan().items():
-                topic = self.topic_id + '%s' % beacon
-                self.report(topic, data)
+                if beacon not in self.beacons.keys():
+                    self.beacons[beacon] = {}
+                self.report(beacon, data)
+
+    def update_state(self, beacon, state):
+        if self.mqtt_client:
+            topic = self.topic_id + '%s' % beacon + '/state'
+            self.mqtt_client.publish(topic, state)
+
+    def check_states(self):
+        now = datetime.utcnow()
+        for beacon, data in self.beacons.items():
+            do_update = False
+            if (now - data.get('datetime')).total_seconds() < 120:
+                if data.get('state') != 'home':
+                    state = 'home'
+                    do_update = True
+            else:
+                if data.get('state') != 'not_home':
+                    state = 'not_home'
+                    do_update = True
+            if do_update:
+                self.update_state(beacon, state)
+                data['state'] = state
+
 
 if __name__ == '__main__':
     beacon_report = BeaconReport('config')
     while True:
         beacon_report.do_scan()
+        beacon_report.check_states()
